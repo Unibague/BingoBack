e46837450fbfa2cd95502191508213c2268f1f09
Jonatan andres <jonatan25sanchez@gmail.com>
2025-08-01 16:32:26 -0500

    agregando sistema de gmail funcional corrigiendo tipado entrando contenedor de base de datos ocnfiguracion de cors.

--- src/main/java/bingo/unibague/demo/Security/WebSecurityConfig.java
+++ src/main/java/bingo/unibague/demo/Security/WebSecurityConfig.java
@@ -83,7 +83,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://172.19.18.21:8085"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
